75f682d007381874cc39066f87389ed59c52aa28
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-08 06:49:46 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict"; //treats all js codes as newer one
+
+// alert(3 + 3) // we are using nodejs not browser
+
+// redability is really important
+
+/*
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+
+
+# "use strict";   treat all code as newer version of JS
+
+
+*/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Bismillah")
+const accountId = 12345
+let accountEmail = 'dfasfdf@gmail.com'
+var accountPassword = "12300"
+accountCity = "Abu Dhabi"
+
+// accountId = 23// Not allowed const value can. not be re assigned
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
