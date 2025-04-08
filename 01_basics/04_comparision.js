88c63621d5c62a7e835751bf42deaab767697a82
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-08 14:19:10 +0000

    comparison operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null != 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined != 0);
+
+console.log("2" === 2); // This is a equality comparision where it compares the typeof compared data types.
+
+
