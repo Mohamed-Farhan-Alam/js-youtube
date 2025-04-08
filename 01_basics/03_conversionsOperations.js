a3e35de95eefd24f4eb502e28c49343019c1d412
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-08 12:09:40 +0000

    conversionOperations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,12 +1,13 @@
+/******************************************Conversions***********************************************/
 let score = "33thirtyThree"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+//console.log(valueInNumber);
 
 /*
   In data conversions "33" will convert to 33 as a number
@@ -18,9 +19,9 @@ console.log(valueInNumber);
 let loggedIn = 0
 
 let booleanInLoggedIn = Boolean(loggedIn)
-console.log(booleanInLoggedIn);
-console.log(typeof loggedIn);
-console.log(typeof booleanInLoggedIn);
+//console.log(booleanInLoggedIn);
+//console.log(typeof loggedIn);
+//console.log(typeof booleanInLoggedIn);
 
 // any number 0 = false 1 = true
 // string = true
@@ -29,10 +30,43 @@ console.log(typeof booleanInLoggedIn);
 let someNumber = 786
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
 
 // here even number can also be converted to strings
+
+
+
+// *************************************************Operations***********************************************
+
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Bismillah"
+let str2 = "HirRehman"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+// console.log("1" + (2 + 3));
+
+// different between prefix and post fix increment
+
+// let x = 3;
+// let y = x++;
+// console.log(x, y);
+
+// let x1 = 3;
+// let y1 = ++x1;
+// console.log(x1, y1);
+ 
+
 
 
 
