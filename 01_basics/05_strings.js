63e5b119772c9e066fb4444163262ec32c81657b
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-11 07:45:52 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+// string Interpolation ${ }
+
+let name = "Farhan"
+let age = 30
+
+console.log(name[3]);
+
+
+console.log(`Hi my name is ${name} and my age is ${age}.`);
+
+//another method of declaring a string
+
+let gameName = new String("Arabia")
+
+//console.log(gameName[5]);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.substring(0, 4));
+
+
+console.log(gameName.slice(-8, 5));
+
+let url = "https://humble-umbrella-4j6v4p5q556q35x67.github.dev/"
+
+console.log(url.replace('556', '786'));
+
+
+
+
+
+
+
+
