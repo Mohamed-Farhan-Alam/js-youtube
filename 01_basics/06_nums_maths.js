7bef0132b85a12ee49cd9965a6dea92fd3a99ae5
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-12 07:48:13 +0000

    Math and Num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,35 @@
+let score = 100
+//console.log(score);
+
+let balance = new Number(1000)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2).length);
+// console.log(balance.toPrecision(3));
+
+const otherNumber = 1000.123456789
+//console.log(otherNumber.toPrecision(6));
+
+const hunreds = 1000000000000000
+// console.log(hunreds.toLocaleString());
+// console.log(hunreds.toLocaleString('en-IN'));
+
+
+//************************************************************Maths************************************************ */
+
+// console.log(Math);
+// console.log(Math.abs(-100));
+// console.log(Math.ceil(100.1));
+// console.log(Math.floor(100.4));
+// console.log(Math.round(100.4));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10) + 1));
+
+const Min = 10
+const Max = 20
+
+console.log(Math.floor(Math.random()*(Max - Min + 1))+Min);
