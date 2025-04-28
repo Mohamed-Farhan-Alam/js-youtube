1a60f970b6ca41200b388b7147f39a299714a50e
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-04-28 18:50:43 +0000

    arrat

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,61 @@
+let myArry = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+const myHeros = ['Khalid-bin-waleed', 'Ali-bin-abi-talib', 'Abu-bakr', 'Umar-bin-khattab']
+
+const myArry2 = new Array(10, 20, 30, 50)
+// console.log(myArry[5]);
+
+// Array Methods
+
+// myArry.push("king")
+
+// myArry.pop()
+
+// myArry.unshift("first")
+// myArry.shift()
+
+// console.log(myArry.includes(25));
+// console.log(myArry.indexOf(5));
+
+
+// const newArry = myArry.join() // Thiwill turn this array into a string
+// console.log(newArry);
+// console.log(typeof newArry);
+
+
+
+// slice and splice
+
+
+//  const myn1 = myArry.slice(1, 5) 
+//  console.log("A ", myArry);
+//  console.log(myn1);
+
+
+//  const myn2 = myArry.splice(1, 5) 
+//  console.log("B ", myArry);
+//  console.log(myn2);
+
+ const myHeros2 = ["Mehmet", "Osman", "Faisla", "Obeida"]
+
+ //myHeros.push(myHeros2)
+    //console.log(myHeros);  // here, one whole array is pushed into a new array as another element of the array.
+
+// myAll_Heros = myHeros.concat(myHeros2)
+// console.log(myAll_Heros); // here two arrays are concatenated into one array.
+ 
+// // Alternative to concat is spread operator here just use 3 dots to concat 2 or more arrays.
+
+// const myAll_Heros2 = [...myHeros, ...myHeros2]
+// console.log(myAll_Heros2); // here two arrays are concatenated into one array.
+
+// const crazy_Array = [1, 2, 3, [3, 5, 8], 12, 26, 10, [[[2, 3, 5], 5, 8], 12, 26, 10]]
+//  const unCrazy_Array = crazy_Array.flat(Infinity)    // this will remove the nested arrays and make it a single array.
+//  console.log(unCrazy_Array);
+
+
+   
+
+
+
+
+
