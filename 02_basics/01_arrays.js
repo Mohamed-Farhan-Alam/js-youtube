7d13fa218f7819a8b02d2499ec659ff7b07a9be7
Mohamed-Farhan-Alam <farhanalamhr@gmail.com>
2025-05-04 22:38:06 +0000

    Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -43,7 +43,7 @@ const myArry2 = new Array(10, 20, 30, 50)
 // myAll_Heros = myHeros.concat(myHeros2)
 // console.log(myAll_Heros); // here two arrays are concatenated into one array.
  
-// // Alternative to concat is spread operator here just use 3 dots to concat 2 or more arrays.
+// // Alternative to concat is spread operator here just use 3 dots to concatenate 2 or more arrays.
 
 // const myAll_Heros2 = [...myHeros, ...myHeros2]
 // console.log(myAll_Heros2); // here two arrays are concatenated into one array.
@@ -53,7 +53,33 @@ const myArry2 = new Array(10, 20, 30, 50)
 //  console.log(unCrazy_Array);
 
 
-   
+Array.isArray("Yemen") // this will check if the given value is an array or not.
+console. log (Array.isArray("Yemen")); // this will check if The given value is an array or not.
+
+  
+
+// const crazy_Array = [1, 2, 3, [3, 5, 8], 12, 26, 10, [[[2, 3, 5], 5, 8], 12, 26, 10]]
+//  const unCrazy_Array = crazy_Array.flat(Infinity)    // this will remove the nested arrays and make it a single array.
+//  console.log(unCrazy_Array);
+
+
+
+console.log(Array.from("Yemen")); // this will convert the string into an array of characters
+
+
+console.log(Array.from({name: "Yemen", age: 22})); // this will convert the object into an array of key-value pairs This is interesting and for future
+console.log(Array.from({length: 5})); // this will create an array of length 5 with undefined values
+console.log(Array.from({length: 5}, (value, index) => index + 1)); // this will create an array of length 5 with values from 1 to 5
+console.log(Array.from({length: 5}, (value, index) => index + 1)); // this will create an array of length 5 with values from 0 to 4
+console.log(Array.from({length: 5}, (value, index) => index + 1).map((value) => value * 2)); // this will create an array of length 5 with values from 0 to 4 and then multiply each value by 2
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+let score4 = 400
+
+console.log(Array.of(score1, score2, score3, score4)); // this will create an array of the given values
 
 
 
